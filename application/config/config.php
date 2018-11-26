6559cf24baa4c84e65ad369ba415995073876114
Nerwin <aleluyaerwin@gmail.com>
2018-11-26 18:16:28 -0400

    cambios en _sec

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] =  'http://172.16.0.48/archivoFinanciero/';
+$config['base_url'] =  'http://localhost/archivoFinanciero/';
 
 /*
 |--------------------------------------------------------------------------
